f7d527aef353de28114f26e5f9780428521de406
Stephen Drouet <davidrouet009@gmail.com>
2025-07-25 05:28:07 +0000

    mueve las credenciales de firebase a un .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmcC-ahcx4VB3z0Jgkz7Se-QsrJGaJjcI",
-  authDomain: "collabtask-58111.firebaseapp.com",
-  projectId: "collabtask-58111",
-  storageBucket: "collabtask-58111.firebasestorage.app",
-  messagingSenderId: "516505999289",
-  appId: "1:516505999289:web:7cfa3c32caf0bfedd38868"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
